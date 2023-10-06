6853801cf317bdb06c54d760d7a6c9127f7f5478
eannoy <fajarrohino14@gmial.com>
2023-10-06 17:33:27 +0700

    integration cs and kape, home

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,17 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Center, Text } from "@chakra-ui/react";
+import { GoCopilot } from "react-icons/go";
 
 export default function Home() {
   return (
     <>
-      <Box>
-        <Text>Welcome to Pijat Online</Text>
-      </Box>
+      <Center>
+        <Box display={"flex"} flexDirection={"column"} h={"100vh"} w={"100vh"} justifyContent={"center"} alignItems={"center"}>
+          <GoCopilot color={"teal"} fontSize={"4em"} />
+          <Text fontSize={"xl"} fontWeight={"bold"} color={"teal"}>
+            Welcome to Pijat Online
+          </Text>
+        </Box>
+      </Center>
     </>
   );
 }
