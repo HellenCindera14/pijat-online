69c499ef2ea4afc517dbaaba3f61d5762247940a
Fajar Rohino <fajarrohino14@gmial.com>
2023-10-06 13:42:11 +0700

    initial client

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <>
+      <Box>
+        <Text>Welcome to Pijat Online</Text>
+      </Box>
+    </>
+  );
+}
