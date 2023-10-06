63408bb316d2d388312875bdd12b11b1f6468b11
unknown <aderino.2232510384@gmail.com>
2023-10-06 14:07:51 +0700

    feat: update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
+import SellerDashboard from './pages/SellerDashboard';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Seller-Dashboard" element={<SellerDashboard />} />
       </Routes>
     </>
   );
---
+++ client/src/pages/SellerDashboard.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export default function SellerDashboard() {
+  return (
+    <>
+      <Box>
+        <Text>Welcome to Pijat Online</Text>
+      </Box>
+    </>
+  );
+}
