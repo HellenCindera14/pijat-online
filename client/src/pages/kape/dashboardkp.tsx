b6578b5b0bb1504a9e3dbcca806b7953ef2c2431
helenBelajar <helencindera@gmail.com>
2023-10-08 02:18:42 +0700

    +layoutePage

--- client/src/pages/kape/dashboardkp.tsx
+++ client/src/pages/kape/dashboardkp.tsx
@@ -12,7 +12,7 @@ export default function Dashboardkp() {
       <LayoutPageKp>
         <Stack mt={"7.5vh"} spacing={4}>
           <Card>
-            <Box ps={6} pt={6} >
+            <Box ps={6} pt={6}>
               <Heading size="sm" textAlign={"center"}>
                 Dashboard
               </Heading>
