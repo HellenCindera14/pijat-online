c02703da2ceaf3bd644149b90c89826c88aee503
BagasSatr3 <bagassatjin23@gmail.com>
2023-10-07 20:26:47 +0700

    fix: typo data-source.ts

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,14 +3,15 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "aws.connect.psdb.cloud",
     port: 3306,
-    username: "root",
-    password: "",
-    database: "hebajarino",
-    synchronize: true,
+    username: "igbw8i274a37mbbnilaa",
+    password: "pscale_pw_dIzNXLpHQ5aP73smgo7qh79zl0EsVjiYOHUB06GTWJi",
+    database: "setsuzu",
+    synchronize: false,
     logging: true,
     entities: ["src/entities/*.ts"],
     migrations: ["src/migrations/*.ts"],
     subscribers: [],
+    ssl: {}
 });
